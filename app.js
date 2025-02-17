3225e7ec878b53cad8c7e34495631e74a93b1839
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-17 19:37:05 +0100

    end exercise 14/02

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express()
 port=3000
 //NOTE - importiamo le rotte dal file esterno
 const postsRouter = require('./routers/posts');
+//NOTE - Importiamo il middleware not found
+const notFound = require ("./middleware/notFound")
+//NOTE - Importiamo il middleware error handler
+const errorsHandler = require("./middleware/errorsHandler")
+
+
+app.use(errorsHandler)
 //NOTE - con questa sintassi attiviamo la decodifica (in questo caso) dei file json passati tramite richiesta del body senza di questa viene restiruito undefind
 //NOTE - attenzione alla posizione dove vai ad inserirla perchè se sta sotto alle routers non viene letta e continua a darti undefind
 //NOTE - con app use abbiamo uno scope globale anche per i file esterni all'app
@@ -14,7 +21,8 @@ app.use(express.json())
 app.use("/public",express.static("public"))
 //NOTE - ablitiamo la rotta per accedere alle rotte
 app.use('/posts', postsRouter);
-
+//NOTE - sempre attenzione alla posizione se lo metti sopra all'applicativo si blocca tutto darà sempre il messaggio
+app.use(notFound);
 // questo codice avvia un server web e lo mette in ascolto su una porta specifica, pronta a gestire le richieste HTTP e stampa in console Example app listening ON http://localhost:${port}
 app.listen(port, () => {
     console.log(`Example app listening ON http://localhost:${port}`)
